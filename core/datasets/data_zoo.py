7dce57cdf018a8b2acdb1d7291dfbc33be6dde78
StephenTerror <2684109034@qq.com>
2021-05-09 18:16:00 +0800

    update

--- core/datasets/data_zoo.py
+++ core/datasets/data_zoo.py
@@ -4,12 +4,12 @@ from . import data
 def get_data_by_name(name: str, **kwargs):
     if name.islower() and not name.startswith("_"):
         data_file = data.__dict__[name]
-        train_dataset = data_file.classify_train_dataset(**kwargs)
-        val_dataset = data_file.classify_val_dataset(**kwargs)
-        test_dataset = data_file.classify_test_dataset(**kwargs)
+        train_dataset = data_file.train_dataset(**kwargs)
+        val_dataset = data_file.val_dataset(**kwargs)
+        test_dataset = data_file.test_dataset(**kwargs)
     else:
-        print("[ERROR] Data name you selected is not support, but can be registered.")
-        print("[WARNING] Custom dataset loading file should be add in 'core/dataset/data/*', "
+        print(f"[ERROR] Data name you selected '{name}' is not support, but can be registered.")
+        print("[WARNING] Custom dataset should be added into 'core/dataset/data/*', "
               "and import in file 'core/dataset/data/__init__.py'.")
         raise NameError
 
